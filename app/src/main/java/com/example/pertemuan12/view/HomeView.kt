2623db42f55a15f9b48b2837856731f5e89dae8e
DimasCW <cdimas292@gmail.com>
2024-12-28 22:46:24 +0700

    membuat file HomeView.kt dan memberi parameter function HomeScreen

---
+++ app/src/main/java/com/example/pertemuan12/view/HomeView.kt
@@ -0,0 +1,25 @@
+package com.example.pertemuan12.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.ui.navigation.DestinasiNavigasi
+import com.example.pertemuan12.viewModel.HomeViewModel
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToltemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+
+){
+
+}
