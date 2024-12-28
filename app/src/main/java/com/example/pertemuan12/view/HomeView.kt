41765d74c9bc3f16a13e0552b5b20c65395adc51
DimasCW <cdimas292@gmail.com>
2024-12-28 23:06:58 +0700

    membuat function OnError

--- app/src/main/java/com/example/pertemuan12/view/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/view/HomeView.kt
@@ -2,13 +2,16 @@ package com.example.pertemuan12.view
 
 import android.graphics.Paint.Align
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -20,8 +23,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan12.R
 import com.example.pertemuan12.model.Mahasiswa
 import com.example.pertemuan12.ui.costumwidget.CostumeTopAppBar
 import com.example.pertemuan12.ui.navigation.DestinasiNavigasi
@@ -125,3 +131,20 @@ fun OnLoading(modifier: Modifier = Modifier){
 
     )
 }
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = "")
+        Text(text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction){
+            Text(stringResource(R.string.retry))
+        }
+        }
+    }
