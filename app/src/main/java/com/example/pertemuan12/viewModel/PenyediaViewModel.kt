4acfc827a0388042fa68a8331091b5056bb1af88
DimasCW <cdimas292@gmail.com>
2024-12-30 12:18:01 +0700

    menambah code pada PenyediaViewModel.kt

--- app/src/main/java/com/example/pertemuan12/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewModel/PenyediaViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.pertemuan12.viewModel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan12.MahasiswaApplications
 
 
-object PenyediaViewModel {
+object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(
@@ -19,8 +20,17 @@ object PenyediaViewModel {
                 aplikasiMahasiswa().container.mahasiswaRepository
             )
         }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+        initializer {
+            UpdateViewModel(
+                createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
     }
+    fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as MahasiswaApplications)
 }
-
-fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
